249cb1ee00b50d324d04a974b0f7437dd4e3384a
bamboohiko <bamboohiko@163.com>
2017-10-06 16:00:46 +0800

    20171006 1600

--- dicebot.py
+++ dicebot.py
@@ -14,32 +14,44 @@ req.headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.
 def dicebot(item):
     #sender = item.fetchPreviousSiblings()[0]
     #text = item.text.replace('#r ','',1)
+    command = ''
+    reply = ''
+
     sender = '没有'
+    pattern = re.compile(r'#r\s*(([0-9]*)d([0-9]*)\+?)+\s*(?P<hint>\S*)')
+    match = pattern.match(item)
+    if not match:
+        reply = '命令格式错误'    
+    else:
+        hint = match.group(4);
+        pattern = re.compile(r'((?P<x>[0-9]*)d(?P<y>[0-9]*)\+?)')
+        coms = pattern.findall(item[:match.start(4)])
+
+        comsum = 0
+        
+        for com in coms:
+            print(com)
+            x = (com[1] == '' and 1) or int(com[1])
+            y = (com[2] == '' and 100) or int(com[2])
+            if (x < 1 or x > 100 or y < 1 or y > 10000):
+                command = ''
+                reply = '参数错误'
+                break
+            command += str(x) + 'd' + str(y) + '+'
+            reply += ' ('
+            for i in range(x):
+                j = random.randint(1,y)
+                comsum += j
+                reply += str(j) + ','
+            reply = reply[:-1] + ') +'
     
-    text = item.replace('#r ','',1)
-    coms = text.split("+")
-    reply = ""
-    comsum = 0
-    for com in coms:
-        com = ' ' + com
-        if com.split('d')[0].isspace():
-            x = 1
-        else:
-            x = int(com.split('d')[0])
-        y = int(com.split('d')[1])
-        print(x,y)
-        reply += ' ('
-        for i in range(x):
-            j = random.randint(1,y)
-            comsum += j
-            reply += str(j) + ','
-        reply = reply[:-1] + ') +'
-    reply = reply[:-2] + ' = ' + str(comsum)
-    reply = '* ' + sender + ' 投掷 :' + text + ' =' + reply; 
+    if command != '':
+        reply = reply[:-2] + ' = ' + str(comsum)
+        reply = '* ' + sender + ' 投掷 ' + hint + ': ' + command[:-1] + ' =' + reply; 
     
     #send(reply)
-    print(reply)
+    #print('收到命令：',text,'已回复:',reply)
     
-    print('收到命令：',text,'已回复:',reply)
+    print(reply)
 
-dicebot('#r 2d100+d100')
+dicebot('#rabdfaf')
